8fad6d34fadac15ca391294d3fe344933f9d6cde
Samarth <Varahi1917115@gmail.com>
2022-04-19 20:11:05 -0400

    commit tree fff3af35404dee18143c7c67b6513ceb947e9e96

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+
+main.remove(main);
+
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+
+newHeader.textContent = "Michael is the champion"
+document.body.append(newHeader)
